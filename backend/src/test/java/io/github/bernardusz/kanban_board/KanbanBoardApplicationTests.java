e74d8295c3958753bed92d04b30000f2d5321d28
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-05-23 09:41:01 +0700

    Initial commit: Scaffolded Java 25 backend & Analog.js frontend

---
+++ backend/src/test/java/io/github/bernardusz/kanban_board/KanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bernardusz.kanban_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
