67cda65df9630779756c803c32b523234d5c45fe
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-05-26 18:23:23 +0700

    feat: Added TaskRepository and TaskService
    - Added TaskRepository for interacting with the DB via JdbcClient
    - Added TaskService for interacting with the TaskRepository

--- backend/src/main/java/io/github/bernardusz/kanban_board/kanban/TaskRepository.java
+++ backend/src/main/java/io/github/bernardusz/kanban_board/kanban/TaskRepository.java
@@ -1,3 +1,38 @@
 package io.github.bernardusz.kanban_board.kanban;
 
-public class TaskRepository {}
+import io.github.bernardusz.kanban_board.kanban.dto.TaskCreateRequest;
+import io.github.bernardusz.kanban_board.kanban.dto.TaskSummary;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TaskRepository {
+  private final JdbcClient jdbcClient;
+  public TaskRepository(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
+  }
+
+  public List<TaskSummary> findAll() {
+    return jdbcClient.sql("SELECT id, title, status, created_at, updated_at FROM tasks").query(TaskSummary.class).list();
+  }
+
+  public Task findById(long id) {
+    return jdbcClient.sql("SELECT * FROM task WHERE id = ?").param(id).query(Task.class).single();
+  }
+
+  public void create(TaskCreateRequest task){
+    jdbcClient.sql("INSERT INTO tasks (title, description, status) VALUES (?, ?, ?)")
+      .params(task.title(), task.description(), task.status()).update();
+  }
+
+  public void update(Task task) {
+    jdbcClient.sql("UPDATE tasks SET title = ?, description = ?, status = ? WHERE id = ?")
+      .params(task.title(), task.description(), task.status(), task.id()).update();
+  }
+
+  public void delete(long id) {
+    jdbcClient.sql("DELETE FROM tasks WHERE id = ?").param(id).update();
+  }
+}
--- backend/src/main/java/io/github/bernardusz/kanban_board/kanban/TaskService.java
+++ backend/src/main/java/io/github/bernardusz/kanban_board/kanban/TaskService.java
@@ -1,3 +1,41 @@
 package io.github.bernardusz.kanban_board.kanban;
 
-public class TaskService {}
+import io.github.bernardusz.kanban_board.kanban.dto.TaskCreateRequest;
+import io.github.bernardusz.kanban_board.kanban.dto.TaskSummary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+  private final TaskRepository taskRepository;
+  public TaskService(TaskRepository taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
+  @Transactional
+  public List<TaskSummary> findAll() {
+    return taskRepository.findAll();
+  }
+
+  @Transactional
+  public Task findById(long id) {
+    return taskRepository.findById(id);
+  }
+
+  @Transactional
+  public void createTask (TaskCreateRequest newTask) {
+    taskRepository.create(newTask);
+  }
+
+  @Transactional
+  public void updateTask(Task task) {
+    taskRepository.update(task);
+  }
+
+  @Transactional
+  public void deleteTask(long id) {
+    taskRepository.delete(id);
+  }
+}
