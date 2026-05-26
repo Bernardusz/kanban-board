582ba14964b78cfc97c1fbf7f359821519b09906
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-05-26 17:48:43 +0700

    feat: Scaffolded Spring Boot App and Edited tsconfig.json
    - Scaffolded Spring Boot project with the new Package by feature structure
    - Edited tsconfig.json to remove deprecated input for TS 7.0

---
+++ backend/src/main/java/io/github/bernardusz/kanban_board/kanban/TaskController.java
@@ -0,0 +1,3 @@
+package io.github.bernardusz.kanban_board.kanban;
+
+public class TaskController {}
---
+++ backend/src/main/java/io/github/bernardusz/kanban_board/kanban/TaskStatus.java
@@ -0,0 +1,8 @@
+package io.github.bernardusz.kanban_board.kanban;
+
+public enum TaskStatus {
+  TODO,
+  PROGRESS,
+  REVIEW,
+  DONE
+}
